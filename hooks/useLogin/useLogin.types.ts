11731413ec32bb18ce39409f2b999dbb9a320e67
Lorenzo Hiver <lorenzohiver1@gmail.com>
2023-12-12 20:25:57 +0100

    feature(app): new pages

--- hooks/useLogin/useLogin.types.ts
+++
@@ -1,6 +0,0 @@
-import { type z } from 'zod'
-import { type loginDataSchema, type apiResponseSchema } from './useLogin.schema'
-
-export type ApiResponseData = z.infer<typeof apiResponseSchema>
-
-export type LoginData = z.infer<typeof loginDataSchema>
